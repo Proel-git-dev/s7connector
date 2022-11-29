f21f73fae8803edc2a6e72527af71e295d5a9a5e
pavle.sprljan <pavle.sprljan@proel.hr>
2022-11-29 13:02:30 +0100

    Izdvajanje klase za upravljanje konekcijom prema PLC-u (S7commManager) Petlja s command promptom po uzoru na C# primjer

--- src/test/java/com/github/s7connector/test/example/PlainTutorialExample.java
+++ src/test/java/com/github/s7connector/test/example/PlainTutorialExample.java
@@ -15,10 +15,6 @@ limitations under the License.
 */
 package com.github.s7connector.test.example;
 
-import com.github.s7connector.api.DaveArea;
-import com.github.s7connector.api.S7Connector;
-import com.github.s7connector.api.factory.S7ConnectorFactory;
-
 /**
  * @author Thomas Rudin (thomas@rudin-informatik.ch)
  *
@@ -32,39 +28,63 @@ public class PlainTutorialExample
 	
 	public static void main(String[] args) throws Exception
 	{
-		//Open TCP Connection
-		S7Connector connector = 
-				S7ConnectorFactory
-				.buildTCPConnector()
-				.withHost("192.168.100.222")
-				.withRack(0)
-				.withSlot(0)
-				.build();
-		
-		//Set some bytes
-		byte[] bw = new byte[32];
-                // sedamnaesti bajt nije povezan s nicim u PLC programu, sluzi samo za test
-                java.util.Random rand = new java.util.Random();
-                bw[16] = (byte)rand.nextInt();
-		
-		//Write to DB51 32 bytes
-		connector.write(DaveArea.DB, 51, 0, bw);
+		S7commManager S7c = new S7commManager("192.168.100.222");
                 
-                //Read from DB50 32 bytes
-                // ovo je povratni slog prema serveru, sadrzavat ce informacije s PLC-a
-		byte[] br = connector.read(DaveArea.DB, 50, 32, 0);
+                boolean end = false;
+                boolean g = false;
+                boolean s = false;
                 
+                while (!end)
+                {
+                    S7c.SendMessageToPLC();
+                    byte[] br = S7c.ReadDBfromPLC();
+                    S7commManager.PLCstatusWordElements S = S7c.DecodePLCstatusWord(br[0], br[1]);
+                    
+                    System.out.println("PLC status:");
+                    if (S.VP2_uklop)
+                        System.out.println("VP2 uklopljen");
+                    else
+                        System.out.println("VP2 isklopljen");
+                    
+                    if (S.GEN_u_zaletu)
+                        System.out.println("Generator u zaletu");
+                    else
+                        System.out.println("Generator nije u zaletu");
+                    
+                    if (S.GEN_pod_teretom)
+                        System.out.println("Generator pod teretom");
+                    else
+                        System.out.println("Generator nije pod teretom");
+                    
+                    
+                    System.out.println("Naredba GEN:  " + String.valueOf(g));
+                    System.out.println("naredba SYNC: " + String.valueOf(s));
+                    System.out.println("Naredba? g/s/q");
+                    
+                    byte[] b = new byte[1];
+                    System.in.read(b);
+                    
+                    switch (b[0]){
+                        case 'g':
+                            g = !g;
+                            S7c.SetGeneratorBit(g);
+                            break;
+                        
+                        case 's':
+                            s = !s;
+                            S7c.SetSynchroBit(s);
+                            break;
+                            
+                        case 'q':
+                            end = true;
+                            break;
+                    }
+                    
+                    
+                            
+                    
+                }
                 
-                System.out.println("Statusni bajt: " + String.format("0x%02X", br[0]));
-                
-                short w = MakeWordFromBytes(br[27], br[26]);
-                System.out.println("Testna rijec 1: " + String.format("0x%02X", w));
-                w = MakeWordFromBytes(br[29], br[28]);
-                System.out.println("Testna rijec 2: " + String.format("0x%02X", w));
-                
-		
-		//Close connection
-		connector.close();
 	}
 
 }
